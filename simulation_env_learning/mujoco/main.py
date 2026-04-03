cf15e046f6f1b7cde8903b34d2e339e197bda13e
Yan <yanmisaka0502@gmail.com>
2026-04-03 18:44:53 +0800

    add mujoco test

--- simulation_env_learning/mujoco/main.py
+++ simulation_env_learning/mujoco/main.py
@@ -10,29 +10,32 @@ xml = """
     <light diffuse=".5 .5 .5" pos="0 0 3" dir="0 0 -1"/>
     
     <!-- 地面：增加摩擦系数和弹性参数 -->
-    <geom type="plane" size="1 1 1" rgba=".9 0 0 1" friction="0.1"/>
+    <geom type="plane" size="2 2 2" rgba=".9 0 0 1"
+      friction="0.1"
+      euler="0.1 0 0"/>
     
     <body pos="0 0 2"> <!-- 提高初始高度，看得更清楚 -->
       <joint type="free"/>
       <!-- condim="1" 使碰撞变为纯弹性，不粘滞 -->
       <!-- solref="0.005 1" 表示更硬、更有弹性 -->
+      <!-- solimp="0.7 0.9 0.001" 表示更高的弹性和更低的粘滞 -->
       <geom type="sphere" size=".1" rgba="0 .9 0 1" 
             condim="1" 
-            solref="0.005 0.3" 
-            solimp="0.9 0.95 0.001"/>
+            solref="0.005 0.2" 
+            solimp="0.7 0.9 0.001"/>
     </body>
   </worldbody>
 </mujoco>
 """
 
 # m = mujoco.MjModel.from_xml_path('/path/to/mjcf.xml')
-m = mujoco.MjModel.from_xml_string(xml)
-d = mujoco.MjData(m)
+m = mujoco.MjModel.from_xml_string(xml) # 从字符串中创建
+d = mujoco.MjData(m) # 创建数据对象
 
 with mujoco.viewer.launch_passive(m, d) as viewer:
-    start_real = time.time()
+    start_real = time.time() # 记录仿真开始的现实时间
     
-    while viewer.is_running() and (time.time() - start_real) < 30:
+    while viewer.is_running() and (time.time() - start_real) < 30: # 运行30秒
         step_start = time.time()
 
         # 执行物理步进
